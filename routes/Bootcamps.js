646dad0bbc6316efb1bb778cee72cb9cd177bd45
JordanD <jjosephd804@gmail.com>
2023-06-14 14:10:43 -0400

    Express router being used for HTTP request methods

---
+++ routes/Bootcamps.js
@@ -0,0 +1,50 @@
+// Copyright 2023 YOUR NAME HERE
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+const express = require('express');
+const router = express.Router();
+
+//Creating a route with HTTP method
+
+router.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: 'Show all bootcamps',
+  });
+});
+router.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Show bootcamp with ID: ${req.params.id}`,
+  });
+});
+router.post('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: 'Create new bootcamp',
+  });
+});
+router.put('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Update bootcamp ID: ${req.params.id}`,
+  });
+});
+router.delete('/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Delete bootcamp ID:  ${req.params.id}`,
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -14,10 +14,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const bootcamps = require('./routes/Bootcamps');
+
 //Load ENV variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
